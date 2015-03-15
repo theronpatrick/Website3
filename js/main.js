694ec7a97ba9ff7dcbf05e3dcbce6bd47e3c984a
Theron Patrick <theronbpatrick@gmail.com>
2015-03-15 19:05:25 -0400

    Fixed border box thing, set up some content functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
 
+	// TODO: Set on onload stuff to go to /theron if not already there.
+
 
 	console.log("hi");
+
+	// Show correct info when hash change
+	$(window).on("hashchange", function() {
+
+		var hash = window.location.hash;
+
+		// Remove hash part
+		hash = hash.substring(2);
+
+		var url = "views/" + hash + ".html";
+
+		$.ajax({
+			url: url
+		})
+		.then(function(response) {
+			$(".content-container").empty();
+			$(".content-container").html(response);
+		})
+
+	})
 
 
 });
