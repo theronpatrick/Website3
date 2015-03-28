165e2f43953e4a9d4ec695b6335bb2772ecd2754
Theron Patrick <theronbpatrick@gmail.com>
2015-03-28 16:18:25 -0400

    Set up some content

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
 
-	// TODO: Set on onload stuff to go to /theron if not already there.
+	// Go to /theron if not already there.
 
+	var hash = window.location.hash;
 
-	console.log("hi");
+	if (hash == "") {
+		window.location.hash = "/theron";
+	} else {
+		setTileActive();
+		loadContent();
+	}
 
 	// Show correct info when hash change
 	$(window).on("hashchange", function() {
+
+		loadContent();
+
+	})
+
+	function loadContent() {
 
 		var hash = window.location.hash;
 
@@ -14,6 +26,10 @@ $(document).ready(function() {
 		hash = hash.substring(2);
 
 		var url = "views/" + hash + ".html";
+
+		// Find corresponding tile to highlight
+		setTileActive();
+
 
 		$.ajax({
 			url: url
@@ -23,7 +39,28 @@ $(document).ready(function() {
 			$(".content-container").html(response);
 		})
 
-	})
+	}
+
+	function setTileActive() {
+		var hash = window.location.hash;
+
+		// Remove hash part
+		hash = hash.substring(2);
+
+		// Find corresponding tile to highlight
+		var selector = "[href*='" + hash + "']";
+		var href = $(selector);
+		var parent = href.parent();
+
+		console.log("href is " , parent);
+
+		// Remove class from others, add to selected one
+		$(".nav-block").removeClass("active");
+
+		parent.addClass("active");
+	}
+
+	// 
 
 
 });
